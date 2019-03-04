c6df7b0126853fd5be59221cca55d06b2debbaca
Sauce Lisandra <slisandr@e7r1p7.21-school.ru>
2019-03-04 19:26:11 +0300

    -

--- day05/ft_strcmp.c
+++ day05/ft_strcmp.c
@@ -6,17 +6,23 @@
 /*   By: slisandr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/04 17:20:03 by slisandr          #+#    #+#             */
-/*   Updated: 2019/03/04 18:15:42 by slisandr         ###   ########.fr       */
+/*   Updated: 2019/03/04 19:23:44 by slisandr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+char	ft_putchar(char c);
 
 int		ft_strcmp(char *s1, char *s2)
 {
 	int i;
 	
 	i = 0;
-	while (s1[i] != '\0' || s2[i] != '\0')
+	while (!(s1[i] == '\0' && s2[i] == '\0'))
 	{
+		ft_putchar(s1[i]);
+		ft_putchar(' ');
+		ft_putchar(s2[i]);
+		ft_putchar('\n');
 		i += 1;
 	}
 	if (s1[i] == '\0' && s2[i] == '\0')
