c0c14a4a7e06276ddceabda25a1125094cf23d06
Sauce Lisandra <slisandr@e7r1p7.21-school.ru>
2019-03-04 21:33:33 +0300

    strcmp is working! (2)

--- day05/ft_strcmp.c
+++ day05/ft_strcmp.c
@@ -6,24 +6,25 @@
 /*   By: slisandr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/04 17:20:03 by slisandr          #+#    #+#             */
-/*   Updated: 2019/03/04 19:23:44 by slisandr         ###   ########.fr       */
+/*   Updated: 2019/03/04 21:29:50 by slisandr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	ft_putchar(char c);
+void	ft_putchar(char c);
 
 int		ft_strcmp(char *s1, char *s2)
 {
 	int i;
 	
 	i = 0;
-	while (!(s1[i] == '\0' && s2[i] == '\0'))
+	while (!(s1[i] == '\0' || s2[i] == '\0'))
 	{
-		ft_putchar(s1[i]);
-		ft_putchar(' ');
-		ft_putchar(s2[i]);
-		ft_putchar('\n');
-		i += 1;
+		if (s1[i] < s2[i])
+			return (-1);
+		else if (s1[i] > s2[i])
+			return (1);
+		else
+			i += 1;
 	}
 	if (s1[i] == '\0' && s2[i] == '\0')
 		return (0);
--- day05/main.c
+++ day05/main.c
@@ -6,7 +6,7 @@
 /*   By: slisandr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/04 16:24:46 by slisandr          #+#    #+#             */
-/*   Updated: 2019/03/04 19:51:50 by slisandr         ###   ########.fr       */
+/*   Updated: 2019/03/04 21:29:48 by slisandr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,33 +19,43 @@ int		ft_strcmp(char *s1, char *s2);
 
 int		main(int argc, char **argv)
 {
-	char	ptr[5] = "ololo";
+	int		num;
 
+	char	*ptr1;
+	char	*ptr2;
+	ptr1 = argv[2];
+	ptr2 = argv[3];
 	if (argc > 1)
 	{
 		if (ft_strcmp(argv[1], "--putnbr") == 0)
 		{
+			ft_putstr("putNBR is runnung\n");
 			ft_putnbr(atoi(argv[2]));
 			return (0);
 		}
 		else if (ft_strcmp(argv[1], "--putchar") == 0)
 		{
+			ft_putstr("putCHAR is runnung\n");
 			ft_putchar(argv[2][0]);
 			return (0);
 		}
 		else if (ft_strcmp(argv[1], "--putstr") == 0)
 		{
+			ft_putstr("putSTR is runnung\n");
 			ft_putstr(argv[2]);
 			return (0);
 		}
 		else if (ft_strcmp(argv[1], "--strcpy") == 0)
 		{
-			ft_putstr(ft_strcpy(ptr, argv[2]));
+			ft_putstr("strCPY is runnung\n");
+			ft_putstr(ft_strcpy(ptr1, argv[2]));
 			return (0);
 		}
 		else if (ft_strcmp(argv[1], "--strcmp") == 0)
 		{
-			ft_putnbr(ft_strcmp(argv[2], argv[3]));
+			ft_putstr("strCMP is runnung\n");
+			num = ft_strcmp(argv[2], argv[3]);
+			ft_putnbr(num);
 			return (0);
 		}
 	}
