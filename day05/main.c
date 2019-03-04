37c4a1bb7d2bec49fac476f02f085d74ffd29d28
root <root@DESKTOP-TOUI2AD.localdomain>
2019-03-04 14:27:56 +0300

    changed main

--- day05/main.c
+++ day05/main.c
@@ -6,32 +6,15 @@ void	ft_putnbr(int nb);
 
 int	main(int argc, char **argv)
 {
-	ft_putnbr(0);
-	ft_putchar('\n');
-	ft_putnbr(-10);
-	ft_putchar('\n');
-	ft_putnbr(-120);
-	ft_putchar('\n');
-	ft_putnbr(-1000000000);
-	ft_putchar('\n');
-	ft_putnbr(4000);
-	ft_putchar('\n');
-	ft_putnbr('a');
-	ft_putchar('\n');
-	ft_putnbr(300);
-	ft_putchar('\n');
-	ft_putnbr(-50000);
-	ft_putchar('\n');
-	ft_putnbr(-3423423);
-	ft_putchar('\n');
-	ft_putnbr(3242);
-	ft_putchar('\n');
 	if (argc > 1)
 	{
-		ft_putchar(argv[1][0]);
-		ft_putchar('\n');
-		ft_putstr(argv[1]);
-		ft_putchar('\n');
+		if (argv[1] == "-n")
+			ft_putnbr(42);
+		if (argv[1] == "-c")
+			ft_putchar(argv[1][0]);
+		if (argv[1] == "-s")
+			ft_putstr(argv[1]);
+		
 	}
 	return (0);
 }
