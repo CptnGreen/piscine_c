452d332428e3874cb02455a2f71899a9fd4f430e
Sauce Lisandra <slisandr@e7r1p7.21-school.ru>
2019-03-04 23:30:24 +0300

    ?

--- day05/main.c
+++ day05/main.c
@@ -6,7 +6,7 @@
 /*   By: slisandr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/04 16:24:46 by slisandr          #+#    #+#             */
-/*   Updated: 2019/03/04 22:45:16 by slisandr         ###   ########.fr       */
+/*   Updated: 2019/03/04 23:24:39 by slisandr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@ void	ft_putstr(char *str);
 void	ft_putnbr(int nb);
 int		atoi(char *str);
 char	*ft_strcpy(char *dest, char *src);
+char	*ft_strncpy(char *dest, char *sra, unsigned int n);
 int		ft_strcmp(char *s1, char *s2);
 
 int		main(int argc, char **argv)
@@ -45,6 +46,12 @@ int		main(int argc, char **argv)
 		{
 			ft_putstr("strCPY is runnung\n");
 			ft_putstr(ft_strcpy(argv[2], argv[3]));
+			return (0);
+		}
+		else if (ft_strcmp(argv[1], "--strncpy") == 0)
+		{
+			ft_putstr("str-N-CPY is runnung\n");
+			ft_putstr(ft_strncpy(argv[2], argv[3], atoi(argv[4])));
 			return (0);
 		}
 		else if (ft_strcmp(argv[1], "--strcmp") == 0)
