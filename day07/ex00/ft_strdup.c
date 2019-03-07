e6c9151f1bcef5888816cf4654d28693d4df3cea
Sauce Lisandra <slisandr@e8r1p6.21-school.ru>
2019-03-07 16:01:33 +0300

    ex00 fixed

--- day07/ex00/ft_strdup.c
+++ day07/ex00/ft_strdup.c
@@ -1,20 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slisandr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/07 15:52:53 by slisandr          #+#    #+#             */
+/*   Updated: 2019/03/07 16:00:28 by slisandr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
 char	*ft_strdup(char *src)
 {
 	int 	i;
-	int	j;
+	int		j;
 	char	*ptr;
 
 	i = 0;
-	while (src[i++] != '\0')
-	ptr = (char *)malloc(i);
+	while (src[i] != '\0')
+		i++;
+	ptr = (char *)malloc(i + 1);
 	j = 0;
-	while (j < i)
+	while (j <= i)
 	{
 		ptr[j] = src[j];
 		j++;
 	}
-	ptr[++j] = '\0';
-	return ptr;
+	return (ptr);
 }
