0adde5d0a882cd93faaf137a683b7d68fab1e480
Sauce Lisandra <slisandr@e6r1p5.21-school.ru>
2019-03-14 22:09:01 +0300

    -

--- day12/ex02/header.h
+++ day12/ex02/header.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slisandr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/14 22:06:53 by slisandr          #+#    #+#             */
+/*   Updated: 2019/03/14 22:07:29 by slisandr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef HEADER_H
 # define HEADER_H
 
@@ -7,7 +19,8 @@
 
 #include <unistd.h>
 
-int	ft_display_file(int fd);
+int		ft_display_file(int fd);
 void	ft_putstr(char *str);
+int		ft_atoi(cahr *s);
 
 #endif
--- day12/ex02/main.c
+++ day12/ex02/main.c
@@ -1,21 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slisandr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/14 17:36:56 by slisandr          #+#    #+#             */
+/*   Updated: 2019/03/14 22:03:46 by slisandr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "header.h"
 
 int	main(int argc, char **argv)
 {
 	int	fd;
-	int	i;
 
 	if (argc == 1)
 	{
 		ft_putstr("File name missing.\n");	
 		return (1);
 	}
-	i = 1;
-	while (i < argc)
+	else if (argc > 2)
 	{
-		fd = open(argv[i], O_RDONLY);
-		ft_cat(fd, );
-		i++;
+		ft_putstr("Too many arguments.\n");	
+		return (1);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		ft_putstr("open() error.\n");
+		return (1);
+	}
+	ft_tail(fd, );
+	if (close(fd) == -1)
+	{
+		ft_putstr("close() error.\n");
+		return (1);
 	}
 	return (0);
 }
